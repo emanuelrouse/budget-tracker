05a36ab8d41461110be7cde2913ed3a8327ca616
Emanuel Rouse <emanuelrouse@hotmail.com>
2022-01-29 17:15:24 -0500

    static html injected via js

--- js/BudgetTracker.js
+++ js/BudgetTracker.js
@@ -3,12 +3,46 @@ export default class BudgetTracker {
         // grab the main app and store it in the root of this class
         this.root = document.querySelector(querySelectorString);
         // set the innerHTML of the app to equal the BudgetTracker html
-        // .html is a jQuery method?
         this.root.innerHTML = BudgetTracker.html();
+
+        this.root.querySelector('.new-entry').addEventListener("click", () => {
+            this.onDeleteEntryBtnClick();
+        });
+
+        // Load initial data from local storage
+        this.load();
     }
 
     static html() {
+        return `
+                <table class="budget-tracker">
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Description</th>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody class="entries">
 
+            </tbody>
+            <tbody>
+                <td colspan="5" class="controls">
+                    <button type="button" class="new-entry">New Entry</button>
+                </td>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="5" class="summary">
+                        <strong>Total:</strong>
+                        <span class="total">0.00</span>
+                    </td>
+                </tr>
+            </tfoot>
+        </table>
+        `;
     }
 
     static entryHtml() {
